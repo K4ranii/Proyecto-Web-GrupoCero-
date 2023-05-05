e22d1246c7f6c5f1308a1b978554b7f693695faa
k4ranii <user.name>
2023-05-05 02:01:37 -0400

    Se agrega fecha y hora como utilidad para el usuario en index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -264,5 +264,21 @@
     });
   });
   
+  function actualizarReloj() {
+    var now = new Date();
+    var date = now.toLocaleDateString();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    document.querySelector(".date").textContent = date;
+    document.querySelector(".hours").textContent = hours;
+    document.querySelector(".minutes").textContent = minutes;
+    document.querySelector(".seconds").textContent = seconds;
+  }
+  
+  setInterval(actualizarReloj, 1000);
 
 })()
